a20869981ecba3e629620024173ac67ba42cb43b
Sammy Abdulkader <46463233+sabdul2021@users.noreply.github.com>
2020-02-12 16:08:47 -0500

    not ready for the chat

--- mybot.js
+++ mybot.js
@@ -18,6 +18,8 @@ client.on('ready', () => {
     generalChannel.send(attachment);
 });
 
+
+
 client.on('message', (receivedMessage) => {
         if (receivedMessage.author.return == client.user) {
             return;
@@ -25,10 +27,10 @@ client.on('message', (receivedMessage) => {
         // receivedMessage.channel.send("Message received: " + receivedMessage.author.toString() + ": " + receivedMessage.content);
 
         // receivedMessage.react("👍");
-        // // receivedMessage.guild.emojis.forEach(customEmoji => {
-        // //     console.log(`${customEmoji.name} ${customEmoji.id}`)
-        // //     receivedMessage.react(customEmoji);
-        // // });
+        // receivedMessage.guild.emojis.forEach(customEmoji => {
+        //     console.log(`${customEmoji.name} ${customEmoji.id}`)
+        //     receivedMessage.react(customEmoji);
+        // });
         // let customEmoji = receivedMessage.guild.emojis.get("667109410035728412");
         // receivedMessage.react(customEmoji);
 
@@ -38,6 +40,65 @@ client.on('message', (receivedMessage) => {
             helloCommand(receivedMessage);
         }
 });
+
+client.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+  
+    // If the message content starts with "!kick"
+    if (message.content.startsWith('!kick')) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Kick the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           */
+          member.kick('Optional reason that will display in the audit logs').then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          }).catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+        } else {
+          // The mentioned user isn't in this guild
+          message.reply('That user isn\'t in this guild!');
+        }
+      // Otherwise, if no user was mentioned
+      } else {
+        message.reply('You didn\'t mention the user to kick!');
+      }
+    }
+  });
+
+  client.on('message', message => {
+    if (message.content.startsWith("fuck")) {
+        //in second your message will be delete for profanity usage
+        message.delete(1000).then(d_msg => {d_msg.delete(1000)})
+        .catch(err => {
+            message.reply('I was unable to delete the message');
+            console.log(err);
+        })
+        message.reply("fuck").then(d_msg => {d_msg.delete(1000)})
+        .catch(err => {
+            message.reply('I was unable to delete the message');
+            console.log(err);
+        })
+    }
+  });
+
 
 function processCommand(receivedMessage) {
     let fullCommand = receivedMessage.content.substr(1);
@@ -114,20 +175,15 @@ function replyCommand2(goody, receivedMessage) {
 
 
 
-client.login("NjY3MDc4NTkyOTE5NzY1MDMy.XiqPbw.K747SglILiniYyHGKnpPqgm2_V4");
+client.login("NjY3MDc4NTkyOTE5NzY1MDMy.XkRjxw.T99Lz-vU1XeIoy-9LgTLR3tTw8U");
 //sometimes the token should be changed due to session time and limits
 
 
 
 
 /*
-
-function 
-
-
-
-
-
+dev code for mybot.js
+-- it's still in development stage
 
 
 */
